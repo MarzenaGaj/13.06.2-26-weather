3601369c2df054912af06121e85abb757d3eb3c8
MarzenaGaj <marzenagajczewska@gmail.com>
2026-06-14 09:27:56 +0200

    excel files genrator

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ load_dotenv()
 class Config:
     API_KEY = os.getenv("ENV_API_KEY")
     PLACE = os.getenv("ENV_PLACE")
+    WEATHER_FILE=os.getenv("ENV_WEATHER_FILE")
+
 
 
 #sciezka pliku
--- services/openweather_api.py
+++ services/openweather_api.py
@@ -8,22 +8,21 @@ def get_weather():
     PLACE = Config.PLACE
 
     try:
-        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q="
-{PLACE}&appid={API_KEY}&units=metric")
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={PLACE}&appid={API_KEY}&units=metric")
         data = response.json()
 
-    weather = {
-            "name": data.get("name"),
-            "temp": data.get("main").get("temp"),
-            "feels_like": data.get("main").get("feels_like"),
-            "wind_speed": data.get("wind").get("speed"),
-            "pressure": data.get("main").get("pressure"),
-            "humidity": data.get("main").get("humidity"),
-            "clouds": data.get("clouds").get("all"),
-            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        weather = {
+                "name": data.get("name"),
+                "temp": data.get("main").get("temp"),
+                "feels_like": data.get("main").get("feels_like"),
+                "wind_speed": data.get("wind").get("speed"),
+                "pressure": data.get("main").get("pressure"),
+                "humidity": data.get("main").get("humidity"),
+                "clouds": data.get("clouds").get("all"),
+                "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         }
-        print(weather)
+        return weather
     except:
         print("Błąd")
 
